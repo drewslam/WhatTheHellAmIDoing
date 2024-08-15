4d230995d35299b79857eade629ec615d7a990dd
Drewslam <souzaphonix@pm.me>
2024-08-14 22:11:08 -0700

    Database schema and menu

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,24 @@
+#include <iostream>
+#include <iomanip>
 #include "../include/database.h"
 
-int main() {
-    Database db("todolist.db");
+void display_menu() {
+    std::cout << std::setw(14) << std::setfill(' ') << "";
+    std::cout << std::setw(26) << std::setfill('-') << "" << "Menu";
+    std::cout << std::setw(26) << std::setfill('-') << "" << "\n";
+    std::cout << std::setw(24) << std::setfill(' ') << "" << "***";
+    std::cout << " D - Display the list \n";
+    std::cout << std::setw(24) << std::setfill(' ') << "" << "***";
+    std::cout << " A - Add a task \n";
+    std::cout << std::setw(24) << std::setfill(' ') << "" << "***";
+    std::cout << " E - Edit a task \n";
+    std::cout << std::setw(24) << std::setfill(' ') << "" << "***";
+    std::cout << " M - Mark a task completed\n";
+    std::cout << std::setw(24) << std::setfill(' ') << "" << "***";
+    std::cout << " Input your option: ";
+}
 
-    db.open();
-
+void initialize_table(Database& db) {
     const char* create_table_query =
         "CREATE TABLE IF NOT EXISTS TASK ("
         "ID INT PRIMARY KEY NOT NULL, "
@@ -14,15 +28,29 @@ int main() {
         ");";
 
     db.execute(create_table_query, "Table created successfully");
+}
+
+int main() {
+    Database db("todolist.db");
+
+    db.open();
+
+    initialize_table(db);
 
     const char* insert_data_query =
-        "INSERT INTO TASK (ID, DESCRIPTION, DUE_DATE, COMPLETED) "
-        "VALUES (1, 'Finish this application', '2024-12-31', 0);";
+        "INSERT INTO TASK (DESCRIPTION, DUE_DATE, COMPLETED) "
+        "VALUES ('Finish this application', '2024-12-31', 0);";
 
     db.execute(insert_data_query, "Data inserted successfully.");
 
     const char* select_query = "SELECT * FROM TASK;";
     db.query(select_query);
+
+    display_menu();
+    char input;
+    std::cin >> input;
+
+    std::cout << std::endl;
 
     db.close();
 
